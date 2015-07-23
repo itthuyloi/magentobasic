73c85b69748a6774e8c953e01a78988e19caa098
Your Name <you@example.com>
2015-07-23 15:18:12 +0700

    One step checkout

--- app/code/community/BC/Checkout/Block/Onestep/Billing.php
+++ app/code/community/BC/Checkout/Block/Onestep/Billing.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * @author <de.nguyen@bluecomgroup.com>
+ * @date 22/07/2015
+ */
 class BC_Checkout_Block_Onestep_Billing extends Mage_Checkout_Block_Onepage_Billing
 {
 	
---
+++ app/code/community/BC/Checkout/Block/Onestep/Shipping.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @author <de.nguyen@bluecomgroup.com>
+ * @date 23/07/2015
+ */
+class BC_Checkout_Block_Onestep_Shipping extends Mage_Checkout_Block_Onepage_Shipping
+{
+
+}
